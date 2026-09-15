c041bbf70adb8769dbc27b9325ebd81a65942127
MohamadGhanemDev <mohamad.d.ghanem@gmail.com>
2026-09-15 21:41:27 +0300

    Add files via upload

---
+++ scipt.js
@@ -0,0 +1,58 @@
+
+
+/*
+Identifiers
+    Name conventions and rules
+    Reserved words
+*/
+
+// correct
+var user = "Mhmd";
+//wrong space is not valid
+    //var us er = "Mhmd";
+//wrong we cant start bu number
+    //var 1user = "Mhmd";
+
+//we can put number in the middle or at the end
+var us1er1 = "Mhmd";
+
+//we can put underscore anywhere
+var _us_er_ = "Mhmd";
+
+// we can use dollar sign anywhere
+var $us$er$ = "Mhmd";
+
+//any special character other than $ is wrong and we cant put it anywhere
+    //var #us#er# = "Mhmd";
+
+    //var @us@er@ = "Mhmd";
+
+
+
+
+// THE NAME IN IDENTIFIER IS CASE SENSITIVE
+
+var user = "Mhmd"; // this differ
+var User = "Elie";// than this also this
+var useR = "Nico";// differ than this
+
+
+
+//there is reserved words we can't use
+/*
+
+var var = "mmm";
+var if = "cccc";
+var else ="ddd";
+var function = "hello";
+
+*/
+
+// types of naming the identifier camel case , snake , kebab and not kofta...
+
+// camel case
+var userName = "Mhd";
+
+//PascalCase , snake_case , slash/case , Cobra_Case , dash-case , kebab-case ....
+
+console.log(userName);
